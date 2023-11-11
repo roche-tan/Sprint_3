4f3a1ab10158d93e6ac6abc3cb980ffb90805d02
roche-tan <rochelyn.rallestan@gmail.com>
2023-11-11 15:33:53 +0100

    http-client

--- dist/http-client.js
+++ dist/http-client.js
@@ -1,13 +1,13 @@
 "use strict";
 /*Write a program that performs an HTTP GET request to a URL provided to you as the first command-line argument. Write the String contents of each "data" event from the response to a new line on the console (stdout).*/
+const http = require("http");
 const url = process.argv[2];
 // const url = "http://example.com/";
-const http = require("http");
 http
     .get(url, (response) => {
     response.setEncoding("utf8"); //set response to string
     response.on("data", (chunk) => {
-        //listen for data events and print them in parts (chunk)
+        //listen for data events and print them in parts (chunk). Not recomended to manage large data as it is processed in "chunks"
         console.log(chunk);
     });
     response.on("error", (error) => {
