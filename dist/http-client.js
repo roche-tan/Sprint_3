3cb60de100bdad8b0cc339e8cf881c51019f8e48
roche-tan <rochelyn.rallestan@gmail.com>
2023-11-11 22:36:42 +0100

    changed http import http-client

--- dist/http-client.js
+++ dist/http-client.js
@@ -1,9 +1,13 @@
 "use strict";
 /*Write a program that performs an HTTP GET request to a URL provided to you as the first command-line argument. Write the String contents of each "data" event from the response to a new line on the console (stdout).*/
-const http = require("http");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
 const url = process.argv[2];
 // const url = "http://example.com/";
-http
+http_1.default
     .get(url, (response) => {
     response.setEncoding("utf8"); //set response to string
     response.on("data", (chunk) => {
--- src/http-client.ts
+++ src/http-client.ts
@@ -1,6 +1,6 @@
 /*Write a program that performs an HTTP GET request to a URL provided to you as the first command-line argument. Write the String contents of each "data" event from the response to a new line on the console (stdout).*/
 
-const http = require("http");
+import http from 'http'
 const url: string = process.argv[2];
 // const url = "http://example.com/";
 
