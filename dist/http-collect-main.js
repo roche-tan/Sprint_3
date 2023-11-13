735883acb4032480963be21d12c12b6de480d146
roche-tan <rochelyn.rallestan@gmail.com>
2023-11-13 13:05:38 +0100

    http-collect comment

--- dist/http-collect-main.js
+++ dist/http-collect-main.js
@@ -1,12 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.fetchData = void 0;
 // const url = process.argv[2];
 // const url = "http://example.com/";
-const http = require("http");
+// const http = require("http");
+const http_1 = __importDefault(require("http"));
 const fetchData = (url) => {
     return new Promise((resolve, reject) => {
-        http
+        http_1.default
             .get(url, (response) => {
             let data = "";
             response.on("data", (chunk) => {
--- src/http-collect-main.ts
+++ src/http-collect-main.ts
@@ -1,22 +1,24 @@
 // const url = process.argv[2];
 // const url = "http://example.com/";
-const http = require("http");
-export const fetchData = (url: string): Promise<string[]> => {
+// const http = require("http");
+import http from "http";
+
+export const fetchData = (url: string): Promise<string> => {
   return new Promise((resolve, reject) => {
     http
-      .get(url, (response: any) => {
+      .get(url, (response: http.IncomingMessage) => {
         let data = "";
 
-        response.on("data", (chunk: string) => {
+        response.on("data", (chunk: string) => {//this is triggered when a chunkk of data is received
           data += chunk;
         });
 
-        response.on("end", () => {
+        response.on("end", () => { //is triggered when the entire response has been received.
           console.log(data.length);
           console.log(data);
         });
 
-        response.on("error", (error: Error) => {
+        response.on("error", (error: Error) => {//If an error occurs during the response, reject of the Promise is called with the error.
           reject(error);
         });
       })
