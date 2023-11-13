2e31a52d5af5404ca19def67ad40e27465c74023
roche-tan <rochelyn.rallestan@gmail.com>
2023-11-13 15:23:28 +0100

    time-server

--- dist/time-server.js
+++ dist/time-server.js
@@ -1,5 +1,5 @@
 "use strict";
-/*Create a TCP server in Node.js that listens on a port provided as an argument, sending the current date and time in the "YYYY-MM-DD hh:mm" format to each connecting client, and then closes the connection.*/
+/*Create a TCP (Transmission Control Protocol) server in Node.js that listens on a port provided as an argument, sending the current date and time in the "YYYY-MM-DD hh:mm" format to each connecting client, and then closes the connection.*/
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -18,11 +18,13 @@ const getFormatDate = () => {
 exports.getFormatDate = getFormatDate;
 const port = process.argv[2];
 exports.server = net_1.default.createServer((socket) => {
+    //net.createServer method creates a new TCP or IPC server. Called everytime user connects to the server
     let data = (0, exports.getFormatDate)();
-    socket.write(data + "\n");
-    socket.end();
+    socket.write(data + "\n"); //writes the formatted date and time string to the socket
+    socket.end(); //closing connection
 });
 exports.server.listen(port, () => {
+    //listen for the connections on a specific port
     let data = (0, exports.getFormatDate)();
     console.log(data);
     console.log(`Server listening on port ${port}`);
--- src/time-server.ts
+++ src/time-server.ts
@@ -1,4 +1,4 @@
-/*Create a TCP server in Node.js that listens on a port provided as an argument, sending the current date and time in the "YYYY-MM-DD hh:mm" format to each connecting client, and then closes the connection.*/
+/*Create a TCP (Transmission Control Protocol) server in Node.js that listens on a port provided as an argument, sending the current date and time in the "YYYY-MM-DD hh:mm" format to each connecting client, and then closes the connection.*/
 
 import net from "net";
 
@@ -6,7 +6,7 @@ export const getFormatDate = (): string => {
   let date = new Date();
   let year = date.getFullYear();
   let month = ("0" + (date.getMonth() + 1)).slice(-2);
-  let day = ("0" + date.getDate()).slice(-2); 
+  let day = ("0" + date.getDate()).slice(-2);
   let hours = ("0" + date.getHours()).slice(-2);
   let minutes = ("0" + date.getMinutes()).slice(-2);
 
@@ -16,13 +16,15 @@ export const getFormatDate = (): string => {
 const port = process.argv[2];
 
 export const server = net.createServer((socket: net.Socket) => {
+  //net.createServer method creates a new TCP or IPC server. Called everytime user connects to the server
   let data = getFormatDate();
 
-  socket.write(data + "\n");
-  socket.end();
+  socket.write(data + "\n"); //writes the formatted date and time string to the socket
+  socket.end(); //closing connection
 });
 
 server.listen(port, () => {
+  //listen for the connections on a specific port
   let data = getFormatDate();
   console.log(data);
   console.log(`Server listening on port ${port}`);
