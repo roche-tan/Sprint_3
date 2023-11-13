ae83186bf0f5fdd9007c8ec73635bba51c2d4975
roche-tan <rochelyn.rallestan@gmail.com>
2023-11-13 15:59:57 +0100

    http-json-api-server comments

--- dist/http-json-api-server-main.js
+++ dist/http-json-api-server-main.js
@@ -4,43 +4,49 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createHttpServer = void 0;
-const http_1 = __importDefault(require("http"));
-const url_1 = __importDefault(require("url"));
+const http_1 = __importDefault(require("http")); //module to create hhtp
+const url_1 = __importDefault(require("url")); //module to analize request urls
 const parseTime = (time) => {
+    // configures date time to return object with 3 properties
     return {
-        //returns an object with 3 properties
         hour: time.getHours(),
         minute: time.getMinutes(),
         second: time.getSeconds(),
     };
 };
 const unixTime = (time) => {
+    //configures time to unix time
     return { unixtime: time.getTime() };
 };
 const createHttpServer = () => {
     const parseQuery = (urlParsed) => {
+        //process entry requests
         if (urlParsed.pathname === "/api/parsetime") {
+            // if url is /api/parsetime returns parseTime
             return parseTime(new Date(urlParsed.query.iso));
         }
         else if (urlParsed.pathname === "/api/unixtime") {
+            //if url is /api/unixtime returns unixTime"
             return unixTime(new Date(urlParsed.query.iso));
         }
         else {
             return "Enter a valid endpoint";
         }
     };
-    const server = http_1.default.createServer((req, res) => {
+    const server = http_1.default.createServer(
+    // create server
+    (req, res) => {
         if (req.method === "GET") {
-            const parsedUrl = url_1.default.parse(req.url, true);
+            const parsedUrl = url_1.default.parse(req.url, true); //url is a module. parse to get the url of the request as a string. true is used to convert it to object so the values are accessible
             const result = parseQuery(parsedUrl);
             if (typeof result === "string") {
-                // Ruta no válida
-                res.writeHead(400, { "Content-Type": "application/json" });
+                //if is a string, it is not valid
+                res.writeHead(400, { "Content-Type": "application/json" }); //content sent is in jason format
                 res.end(JSON.stringify({ error: "Invalid endpoint" }));
             }
             else {
-                // Ruta válida
-                res.writeHead(200, { "Content-Type": "application/json" });
+                //in this case an object (parseTime and unixTime)
+                res.writeHead(200, { "Content-Type": "application/json" }); //content sent is in jason format
                 res.end(JSON.stringify(result));
             }
         }
--- src/http-json-api-server-main.ts
+++ src/http-json-api-server-main.ts
@@ -1,9 +1,9 @@
-import http from "http";
-import url from "url";
+import http from "http"; //module to create hhtp
+import url from "url"; //module to analize request urls
 
 const parseTime = (time: Date) => {
+  // configures date time to return object with 3 properties
   return {
-    //returns an object with 3 properties
     hour: time.getHours(),
     minute: time.getMinutes(),
     second: time.getSeconds(),
@@ -11,14 +11,18 @@ const parseTime = (time: Date) => {
 };
 
 const unixTime = (time: Date) => {
+  //configures time to unix time
   return { unixtime: time.getTime() };
 };
 
 export const createHttpServer = () => {
   const parseQuery = (urlParsed: url.UrlWithParsedQuery) => {
+    //process entry requests
     if (urlParsed.pathname === "/api/parsetime") {
+      // if url is /api/parsetime returns parseTime
       return parseTime(new Date(urlParsed.query.iso as string));
     } else if (urlParsed.pathname === "/api/unixtime") {
+      //if url is /api/unixtime returns unixTime"
       return unixTime(new Date(urlParsed.query.iso as string));
     } else {
       return "Enter a valid endpoint";
@@ -26,19 +30,20 @@ export const createHttpServer = () => {
   };
 
   const server = http.createServer(
+    // create server
     (req: http.IncomingMessage, res: http.ServerResponse) => {
       if (req.method === "GET") {
-        const parsedUrl = url.parse(req.url as string, true);
+        const parsedUrl = url.parse(req.url as string, true); //url is a module. parse to get the url of the request as a string. true is used to convert it to object so the values are accessible
 
         const result = parseQuery(parsedUrl);
 
         if (typeof result === "string") {
-          // Ruta no válida
-          res.writeHead(400, { "Content-Type": "application/json" });
+          //if is a string, it is not valid
+          res.writeHead(400, { "Content-Type": "application/json" }); //content sent is in jason format
           res.end(JSON.stringify({ error: "Invalid endpoint" }));
         } else {
-          // Ruta válida
-          res.writeHead(200, { "Content-Type": "application/json" });
+          //in this case an object (parseTime and unixTime)
+          res.writeHead(200, { "Content-Type": "application/json" });//content sent is in jason format
           res.end(JSON.stringify(result));
         }
       } else {
