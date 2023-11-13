acce5e18953a98c7be959f7cd45945fce85c4fd7
roche-tan <rochelyn.rallestan@gmail.com>
2023-11-13 15:33:03 +0100

    http-uppercaserer comments

--- dist/http-uppercaserer-main.js
+++ dist/http-uppercaserer-main.js
@@ -5,15 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createUppercaseServer = void 0;
 const http_1 = __importDefault(require("http"));
-const through2_map_1 = __importDefault(require("through2-map"));
+const through2_map_1 = __importDefault(require("through2-map")); // provides a simple API to transform data in streams. Used to modify data chunks that flow through a stream.
 const createUppercaseServer = () => {
-    return http_1.default.createServer((req, res) => {
-        if (req.method === "POST") {
+    return http_1.default.createServer(//creates a http server with http.createServer method
+    (req, res) => {
+        if (req.method === "POST") { //check if it is POST request
             req
-                .pipe((0, through2_map_1.default)((chunk) => {
-                return chunk.toString().toUpperCase();
+                .pipe(//pipe method. Takes the incoming request stream and pipe it from a stream to another
+            (0, through2_map_1.default)((chunk) => {
+                return chunk.toString().toUpperCase(); //each chunk is converted to string and changed to uppercase
             }))
-                .pipe(res);
+                .pipe(res); // res sent back to the client though pipe
         }
         else {
             res.writeHead(405, { "Content-Type": "text/plain" });
--- src/http-uppercaserer-main.ts
+++ src/http-uppercaserer-main.ts
@@ -1,17 +1,17 @@
 import http from "http";
-import map from "through2-map";
+import map from "through2-map"; // provides a simple API to transform data in streams. Used to modify data chunks that flow through a stream.
 
 export const createUppercaseServer = () => {
-  return http.createServer(
+  return http.createServer(//creates a http server with http.createServer method
     (req: http.IncomingMessage, res: http.ServerResponse) => {
-      if (req.method === "POST") {
+      if (req.method === "POST") { //check if it is POST request
         req
-          .pipe(
+          .pipe(//pipe method. Takes the incoming request stream and pipe it from a stream to another
             map((chunk: string) => {
-              return chunk.toString().toUpperCase();
+              return chunk.toString().toUpperCase(); //each chunk is converted to string and changed to uppercase
             })
           )
-          .pipe(res);
+          .pipe(res); // res sent back to the client though pipe
       } else {
         res.writeHead(405, { "Content-Type": "text/plain" });
         res.end("Método no permitido");
