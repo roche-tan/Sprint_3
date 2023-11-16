889c8d65f1e8f9e5332224c7ab2fb4a3600c59fc
roche-tan <rochelyn.rallestan@gmail.com>
2023-11-16 12:36:49 +0100

    my-fits-io comments

--- dist/my-first-io.js
+++ dist/my-first-io.js
@@ -23,9 +23,9 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs = __importStar(require("fs")); //to have all expotrs from fs modules. Need it for readFileSync
+const fs = __importStar(require("fs")); //to have all expotrs from file System modules. Need it for readFileSync
 const countLines = (filename) => {
-    const contents = fs.readFileSync(filename); //fs.readFileSync reads the content of the file in a sync way
+    const contents = fs.readFileSync(filename); //fs.readFileSync is a function that  reads the content of the file in a sync way
     const lines = contents.toString().split("\n").length - 1; //contents is converted to string and then separated by character \n and obtains length
     return lines;
 };
--- src/my-first-io.ts
+++ src/my-first-io.ts
@@ -1,7 +1,7 @@
-import * as fs from "fs"; //to have all expotrs from fs modules. Need it for readFileSync
+import * as fs from "fs"; //to have all expotrs from file System modules. Need it for readFileSync
 
 const countLines = (filename: string): number => {
-  const contents = fs.readFileSync(filename); //fs.readFileSync reads the content of the file in a sync way
+  const contents = fs.readFileSync(filename); //fs.readFileSync is a function that  reads the content of the file in a sync way
   const lines = contents.toString().split("\n").length - 1; //contents is converted to string and then separated by character \n and obtains length
   return lines;
 };
